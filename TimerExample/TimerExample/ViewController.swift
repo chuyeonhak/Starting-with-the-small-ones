4327cefd56f676289a60845b788619bd7f2eafa0
first commit <=>
2022-07-17 18:53:34 +0900

    RxTimer

--- TimerExample/TimerExample/ViewController.swift
+++ TimerExample/TimerExample/ViewController.swift
@@ -13,12 +13,23 @@ import RxCocoa
 
 
 class ViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    var viewModel: ViewModel?
     
-    private let timerLabel = UILabel().then {
+    private let remainingTime = UILabel().then {
+        $0.text = "00:00:00"
         $0.font = UIFont.systemFont(ofSize: 15)
     }
     
-
+    private let countTextField = UITextField().then {
+        $0.borderStyle = .roundedRect
+    }
+    
+    private let startButton = UIButton().then {
+        $0.layer.cornerRadius = 15
+        $0.backgroundColor = .green
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,19 +39,52 @@ class ViewController: UIViewController {
     private func commonInit() {
         addComponent()
         setConstraints()
+        setCocoaEvent()
         bind()
     }
     
     private func addComponent() {
-        print(#function)
+        [remainingTime, countTextField, startButton].forEach(view.addSubview)
     }
     
     private func setConstraints() {
-        print(#function)
+        remainingTime.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        countTextField.snp.makeConstraints {
+            $0.top.equalTo(remainingTime.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(100)
+        }
+        
+        startButton.snp.makeConstraints {
+            $0.leading.equalTo(countTextField.snp.trailing).offset(8)
+            $0.centerY.equalTo(countTextField)
+            $0.size.equalTo(30)
+        }
+    }
+    
+    private func setCocoaEvent() {
     }
     
     private func bind() {
-        print(#function)
+        let input = ViewModel.Input(remainingTime: startButton.rx.tap
+            .take(1)
+            .filter{ [unowned self] in countTextField.text?.isEmpty == false }
+            .map { [unowned self] in countTextField.text! }),
+        viewModel = ViewModel(input: input)
+        
+        self.viewModel = viewModel
+        
+        viewModel.output?.timeCount
+            .drive(remainingTime.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.output?.timeOver
+            .drive { [unowned self] _ in bind(); print("timeover") }
+            .disposed(by: disposeBag)
     }
+    
 }
 
--- TimerExample/TimerExample/ViewModel.swift
+++ TimerExample/TimerExample/ViewModel.swift
@@ -6,3 +6,78 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class ViewModel {
+    typealias InputType = Input
+    typealias OutputType = Output
+    typealias DependencyModelType = Model
+    
+    let disposeBag = DisposeBag()
+    var input: InputType?
+    var output: OutputType?
+    let model = Model()
+    
+    init(input: Input) {
+        input.remainingTime
+            .bind { [unowned self] firstTime in
+                let time = Int(firstTime) ?? 0
+                model.timeCount.accept(makeTimeStr(time))
+                timerStart(time - 1)
+            }.disposed(by: disposeBag)
+        
+        output = Output(timeCount: model.timeCount.asDriverOnErrorEmpty(),
+                        timeOver: model.timeOver.asDriverOnErrorEmpty())
+    }
+    
+    private func timerStart(_ firstTime: Int) {
+        Observable<Int>
+            .interval(.seconds(1), scheduler: MainScheduler.instance)
+            .take(until: { $0 == firstTime }, behavior: .inclusive)
+            .map { [unowned self] in makeTimeStr(firstTime - $0) }
+            .bind(to: model.timeCount)
+            .disposed(by: disposeBag)
+    }
+    
+    private func makeTimeStr(_ time: Int) -> String {
+        let hours = time / 3600,
+            minutes = time / 60 % 60,
+            second = time % 60,
+            timeStr = String(format:"%02i:%02i:%02i", hours, minutes, second)
+        
+        if time == 0 {
+            model.timeOver.onNext(())
+        }
+        
+        return timeStr
+    }
+    
+    deinit {
+        print("ViewModel deinit")
+    }
+}
+
+extension ViewModel {
+    struct Model {
+        let timeCount = BehaviorRelay<String>(value: "00:00:00")
+        let timeOver = PublishSubject<Void>()
+    }
+    
+    struct Input {
+        let remainingTime: Observable<String>
+    }
+    
+    struct Output {
+        let timeCount: Driver<String>
+        let timeOver: Driver<Void>
+    }
+}
+
+extension ObservableType {
+    func asDriverOnErrorEmpty() -> Driver<Element> {
+        return asDriver { (error) in
+            return .empty()
+        }
+    }
+}
