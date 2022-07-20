f781344d397a3969be0d23687de2d3955d5d1476
first commit <=>
2022-07-20 14:04:55 +0900

    SwiftUI

---
+++ Combine Practice/SwiftUI_Practice/SwiftUI_Practice/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI_Practice
+//
+//  Created by Chuchu Pro on 2021/12/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MyView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Combine Practice/SwiftUI_Practice/SwiftUI_Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI_Practice
+//
+//  Created by Chuchu Pro on 2021/12/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VStack{
+            ContentView()
+            }
+        }
+    }
+}
